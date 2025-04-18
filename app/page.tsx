8e3cd63d664da77736583d16a89d774f6f570491
enzo.legros <zo.legros@gmail.com>
2025-04-18 10:28:56 +0200

    Connection works

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 "use client"
 
-import { signIn } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
+
 
 export default function Home() {
+  const { data: session} = useSession()
+  if (session)
+    console.log(session)
+
   return (
     <>
       <div className="max-w-[1000px] h-screen mx-auto flex justify-center items-center flex-col gap-2">
         <h1 className="text-8pxl uppercase font-black text-center mb-4">NEXT AUTH</h1>
+        { session ? <><p className="mb-4">Vous êtes connecté en tant que : {session.user?.name}</p><button onClick={async () => await signOut()}>Déconnexion</button></> : <p className="mb-4">Déconnecté</p>}
         <div className="flex items-center gap-2">
-          <button className="bg-gray-300 hover:bg-gray-400 rounded-md p-3" onClick={() => signIn("google")}>
+          <button className="bg-gray-300 hover:bg-gray-400 rounded-md p-3" onClick={async () => await signIn("google")}>
             Se connecter avec Google
           </button>
-          <button className="bg-gray-300 hover:bg-gray-400 rounded-md p-3" onClick={() => signIn("github")}>
+          <button className="bg-gray-300 hover:bg-gray-400 rounded-md p-3" onClick={async () => await signIn("github")}>
             Se connecter avec GitHub
           </button>
         </div>
--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -13,5 +13,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
-  ]
+  ],
+  secret: process.env.SECRET_PERSO as string
 }
